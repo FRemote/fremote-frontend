e70a89bd55428badf6955a02b2147dc8bf2217c6
thaycacac <thaycacac@gmail.com>
2020-04-24 23:42:49 +0700

    feat: checkin

--- src/views/today/components/Checkin.vue
+++ src/views/today/components/Checkin.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="block">
-    <checkin-screenshot />
-    <!-- <el-timeline>
+    <el-timeline>
       <el-timeline-item
         v-for="(item, index) of timeline"
         :key="index"
@@ -9,21 +8,22 @@
         placement="top"
       >
         <el-card>
-          <h4><el-button type="text" @click="showDetail(index)">View detail</el-button></h4>
-          <p>Check-in at {{ item.timeNeed }}</p>
+          <p v-if="item.createAt" style="color: #909399">Checked at {{ item.createAt }}</p>
+          <el-button type="success" icon="el-icon-check" v-else-if="item.timeNeed" @click="dialogVisible = true">Check-in</el-button>
+          <p v-else style="color: #909399">Waiting...</p>
         </el-card>
       </el-timeline-item>
     </el-timeline>
     <el-dialog
-      title="Detail check-in"
+      title="Check-in"
       :visible.sync="dialogVisible"
-      width="50%"
+      width="70%"
     >
-      <img :src="image" style="width: 100%;">
+      <checkin-screenshot />
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">Cancel</el-button>
       </span>
-    </el-dialog> -->
+    </el-dialog>
   </div>
 </template>
 
@@ -38,31 +38,31 @@ export default {
       timeline: [
         {
           id: 1,
-          timestamp: "8:00AM",
+          timestamp: '8:00AM',
           image: 'https://i.imgur.com/h2beBX4.png',
-          timeNeed: '2019/4/20 20:46',
-          createAt: "2019/4/20 20:46 "
+          timeNeed: '2020/4/25 08:00',
+          createAt: '2020/4/25 08:11'
         },
         {
           id: 2,
-          timestamp: "12:00AM",
+          timestamp: '12:00AM',
           image: 'https://i.imgur.com/h2beBX4.png',
-          timeNeed: "2019/4/21 20:46",
-          createAt: "2019/4/21 20:46"
+          timeNeed: '2020/4/25 12:00',
+          createAt: '2020/4/25 11:50'
         },
         {
           id: 3,
-          timestamp: "14:00PM",
-          image: 'https://i.imgur.com/h2beBX4.png',
-          timeNeed: "2019/4/22 20:46",
-          createAt: "2019/4/21 20:46"
+          timestamp: '14:00PM',
+          image: '',
+          timeNeed: '2020/4/25 20:46',
+          createAt: ''
         },
         {
           id: 4,
-          timestamp: "16:00PM",
-          image: 'https://i.imgur.com/h2beBX4.png',
-          content: "2019/4/23 20:46",
-          createAt: "fsdfsdf"
+          timestamp: '16:00PM',
+          image: '',
+          timeNeed: '',
+          createAt: ''
         }
       ]
     };
