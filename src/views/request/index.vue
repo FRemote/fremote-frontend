479c7af847c03d0fcd666ee265184ed94074868b
thaycacac <thaycacac@gmail.com>
2020-04-24 09:46:58 +0700

    update: request detail

--- src/views/request/index.vue
+++ src/views/request/index.vue
@@ -1,5 +1,66 @@
 <template>
   <div class="app-container">
-    hello
+    <div v-if="user">
+      <el-row :gutter="20">
+        <el-col :span="6" :xs="24">
+          <user-card :user="user" />
+        </el-col>
+        <el-col :span="18" :xs="24">
+          <el-card>
+            <el-tabs v-model="activeTab">
+              <el-tab-pane label="Detail" name="activity">
+                <activity />
+              </el-tab-pane>
+              <el-tab-pane label="Timeline" name="timeline">
+                <timeline />
+              </el-tab-pane>
+              <el-tab-pane label="Account" name="account">
+                <account :user="user" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-card>
+        </el-col>
+
+      </el-row>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+import UserCard from './components/UserCard'
+import Activity from './components/Activity'
+import Timeline from './components/Timeline'
+import Account from './components/Account'
+
+export default {
+  name: 'Profile',
+  components: { UserCard, Activity, Timeline, Account },
+  data() {
+    return {
+      user: {},
+      activeTab: 'activity'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+      'roles'
+    ])
+  },
+  created() {
+    this.getUser()
+  },
+  methods: {
+    getUser() {
+      this.user = {
+        name: 'Phạm Ngọc Hòa',
+        role: 'IT Department',
+        email: 'thaycacac@gmail.com',
+        avatar: 'https://lh3.googleusercontent.com/proxy/cHWXvZbjjtJbcQzDl_hOIbIABgc5AALO0IGrXlJpsUJZKhVw2Tq2CYOoo6jNyuejtAJHGqaQkV8-nkydOCfovy8Lcg5oUrB17R7_fa-xFVrYtGE'
+      }
+    }
+  }
+}
+</script>
